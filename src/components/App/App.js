ebd3de7b416ef24b338e240c29154fb1e292f3e1
Andrey Maystrenko <koga-717@yandex.ru>
2022-06-28 19:47:13 +0300

    diploma frontend AboutProject page created

---
+++ src/components/App/App.js
@@ -0,0 +1,93 @@
+import React from "react";
+import "./App.css";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+// import api from "../utils/api";
+// import { CurrentUserContext } from "../contexts/CurrentUserContext";
+import {
+  Route,
+  Switch,
+  // Redirect,
+  withRouter,
+  // useHistory,
+} from "react-router-dom";
+import Login from "../Login/Login";
+import Register from "../Register/Register";
+import AboutProject from "../Main/AboutProject/AboutProject";
+import MoviesCardList from "../Movies/MoviesCardList/MoviesCardList";
+import SavedMovies from "../SavedMovies/MoviesCardList/MoviesCardList";
+import AboutMe from "../Main/AboutMe/AboutMe";
+// import ProtectedRoute from "./ProtectedRoute";
+// import * as mestoAuth from "../utils/moviesAuth";
+
+function App() {
+  return (
+      <div className="page">Test
+        {/* <Header email={email} onSignOut={onSignOut} /> */}
+        {/* <Header /> */}
+        <Switch>
+          <Route exact path="/">
+            <AboutProject />
+          </Route>
+        </Switch>
+        {/* <Switch>
+          <Route exact path="/">
+            <AboutProject />
+          </Route>
+          <Route path="/movies">
+            <MoviesCardList />
+          </Route>
+          <Route path="/saved-movies">
+            <SavedMovies />
+          </Route>
+          <Route path="/profile">
+            <AboutMe />
+          </Route>
+          <Route path="/signin">
+            <Login handleLogin={handleLogin} />
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Register handleRegister={handleRegister} />
+            <Register />
+          </Route>
+          <Route>
+            {loggedIn ? <Redirect to="/main" /> : <Redirect to="/signup" />}
+          </Route>
+        </Switch> */}
+        {/* <Footer /> */}
+        {/* <EditProfilePopup
+          isOpen={isEditProfilePopupOpen}
+          onClose={closeAllPopups}
+          onUpdateUser={handleUpdateUser}
+        />
+        <EditAvatarPopup
+          isOpen={isEditAvatarPopupOpen}
+          onClose={closeAllPopups}
+          onUpdateAvatar={handleUpdateAvatar}
+        />
+        <AddPlacePopup
+          isOpen={isAddPlacePopupOpen}
+          onClose={closeAllPopups}
+          onAddPlace={handleAddPlaceSubmit}
+        />
+        <PopupWithForm
+          title="Вы уверены?"
+          name="delete"
+          saveButtonName="Да"
+          onClose={closeAllPopups}
+          // onSubmit
+        />
+        <ImagePopup card={selectedCard} onClose={closeAllPopups} />
+        <InfoTooltip
+          onClose={closeAllPopups}
+          onSuccess={isSuccessfulRegister}
+          onFailure={isFailuredRegister}
+          successMessage="Вы успешно зарегистрировались!"
+          failureMessge="Что-то пошло не так! Попробуйте еще раз."
+
+          /> */}
+      </div>
+  );
+}
+export default withRouter(App);
---
+++ src/components/Main/AboutProject/AboutProject.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./AboutProject.css";
+
+function AboutProject() {
+    return (
+        <section className="about-project">
+            <h2 className="about-project_title">О проекте</h2>
+            <article className="two-columns">
+                <div className="two-columns__column">
+                    <h2 class="two-columns__brief">Дипломный проект включал 5 этапов</h2>
+                    <div class="two-columns__main-text">
+                        Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.
+                    </div>
+                </div>
+                <div className="two-columns__column">
+                    <h2 class="two-columns__brief">На выполнение диплома ушло 5 недель</h2>
+                    <div class="two-columns__main-text">
+                        У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.
+                    </div>
+                </div>
+            </article>
+            <article className="two-columns">
+                <div className="timeline timeline__schema timeline__backend timeline__backend_schema">1 неделя</div>
+                <div className="timeline timeline__schema timeline__frontend timeline__frontend_schema">4 недели</div>
+            </article>
+            <article className="two-columns">
+                    <div className="timeline timeline__subscrypt timeline__backend">Back-end</div>
+                    <div className="timeline timeline__subscrypt timeline__frontend">Front-end</div>
+            </article>
+        </section>
+    )
+}
+export default AboutProject;
