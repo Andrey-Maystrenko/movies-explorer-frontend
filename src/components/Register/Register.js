e5e81d78d342f811ef6db5eca22826534c98398c
Andrey Maystrenko <koga-717@yandex.ru>
2022-07-15 19:35:28 +0300

    diploma frontend Signup adaptive completed

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,40 +1,52 @@
 import React from "react";
 import "./Register.css";
+import {Link} from "react-router-dom";
 
 function Register() {
     return (
-        <div className="profile__window">
-            <section className="profile__content">
-                <h3 className="profile__title">Добро пожаловать!</h3>
+        <div className="register__window">
+            <section className="register__content">
+                <h3 className="register__title">Добро пожаловать!</h3>
                 <form className="form">
-                    <span className="profile__userdata profile__username-position_register">Имя</span>
+                    <span className="register__userdata register__username-position">Имя</span>
                     <input
-                        className="form__input_register profile__userdata-value"
+                        className="form__input_register register__userdata-value"
                         type="text"
-                        name="profile__username"
+                        name="register__username"
                         minLength="2"
                         maxLength="30"
-                        // required
-                        // onChange={handleEmailChange}
+                    // required
+                    // onChange={handleEmailChange}
                     />
-                    <span className="profile__userdata profile__useremail-position_register">E-mail</span>
+                    <span className="register__userdata register__useremail-position">E-mail</span>
                     <input
-                        className="form__input_register profile__userdata-value"
+                        className="form__input_register register__userdata-value"
                         type="email"
-                        name="profile__useremail"
-                        // required
-                        // onChange={handlePasswordChange}
+                        name="register__useremail"
+                    // required
+                    // onChange={handlePasswordChange}
                     />
-                    {/* <span className="popup__error" id="element__name"></span> */}
+                    <span className="register__userdata register__password-position">Пароль</span>
+                    <input
+                        className="form__input_register register__userdata-value"
+                        type="password"
+                        name="register__password"
+                    // required
+                    // onChange={handlePasswordChange}
+                    />
+                    <span className="register__error" id="register__error">Что-то пошло не так...</span>
                     <button
-                        className="profile__save-button"
+                        className="register__save-button"
                         type="submit"
                     >
-                        <span className="profile__save-button-text">
+                        <span className="register__save-button-text">
                             Редактировать
                         </span>
                     </button>
-                    <a className="profile__save-button-text profile__save-button-text_red" href="/#">Выйти из аккаунта</a>
+                    <p className="register__login-option">
+                        {`Уже зарегистрированы? `}
+                        <Link className="register__login-option register__login-option_link" to="/signin">Войти</Link>
+                    </p>
                 </form>
             </section>
         </div>
